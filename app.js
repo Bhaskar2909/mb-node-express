34500456120a75e009d30b6d9872d7f5298edf7a
Bhaskar <reddybhaskar437@gmail.com>
2021-01-15 00:00:56 -0600

    added the app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Greetings From Bhaskar App')
+})
+
+app.get('/about', (req, res) => {
+    res.send("Accesing the about page")
+  })
+  
+  app.get('/contact', (req, res) => {
+    res.send("accesing the contact page ")
+  })
+  
+  app.get('/help', (req, res) => {
+    res.send(`accessing the help page`)
+  })
+
+  app.get('/help/:topic', (req, res) => {
+    res.send(`your request onn the help page ${req.params.topic}`)
+  })
+  
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
